37fe57a60cc10063099bcb0ada230153323c676b
James Bacon <jbacon@g2g3.digital>
2016-02-17 11:19:03 +0000

    Added back in the 'EnableTransactionManagement' as it didn't cause the problem, it was a data volume issue.

--- base/src/main/java/uk/co/baconi/secure/base/BaseConfiguration.java
+++ base/src/main/java/uk/co/baconi/secure/base/BaseConfiguration.java
@@ -29,11 +29,13 @@ import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.server.Neo4jServer;
 import org.springframework.data.neo4j.server.RemoteServer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 @EnableConfigurationProperties
+@EnableTransactionManagement
 @EnableNeo4jRepositories({
         "uk.co.baconi.secure.base.bag",
         "uk.co.baconi.secure.base.lock",
