2a90dcb1a386d52d7246838f35f7be7763f3a2f9
James Bacon <james@baconi.co.uk>
2016-02-17 18:23:21 +0000

    Included some future ideas in Bag searching.

--- base/src/main/java/uk/co/baconi/secure/base/bag/BagGraphRepository.java
+++ base/src/main/java/uk/co/baconi/secure/base/bag/BagGraphRepository.java
@@ -19,6 +19,13 @@ package uk.co.baconi.secure.base.bag;
 import org.springframework.data.neo4j.repository.GraphRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BagGraphRepository extends GraphRepository<Bag> {
+
+    // List<Bag> findByName(final String name);
+
+    // List<Password> findByWhereFor(final String whereFor);
+
 }
