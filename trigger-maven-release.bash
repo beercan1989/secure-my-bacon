f46ed59ae7a62f5dcc9609fe8e5a719ab93bc723
James Bacon <james@baconi.co.uk>
2016-03-05 15:13:41 +0000

    Spelling mistake correction. Clean up after release prepare.

--- trigger-maven-release.bash
+++ trigger-maven-release.bash
@@ -4,3 +4,6 @@
 ## To be run on a local machine
 ##
 mvn --batch-mode --settings ci/settings.xml release:clean release:prepare
+
+## Remove release backups
+mvn --batch-mode --settings ci/settings.xml release:clean
