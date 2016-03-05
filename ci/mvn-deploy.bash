eee65b40b12b558658c5f6482f36a9131c8a89e0
James Bacon <james@baconi.co.uk>
2016-03-05 13:50:46 +0000

    Correcting a mistake in the securing of the private-key, properly miss typed the password.

--- ci/mvn-deploy.bash
+++ ci/mvn-deploy.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-set +e
+## Exit on non-zero codes
+set -ev
 
 ## Decrypt the encrypted private GPG key
 openssl aes-256-cbc -pass env:GPG_CIPHER_PASSWORD -in ci/gpg/private-key.asc.enc -out ci/gpg/private-key.asc -d
@@ -11,14 +12,17 @@ gpg --homedir ci/gpg --import ci/gpg/private-key.asc
 ## Delete the decrypted private GPG key
 rm -v ci/gpg/private-key.asc
 
+## Do not exit on non-zero codes
+set +e
+
 ## Run the standard maven deploy task
-mvn --batch-mode --settings travis/settings.xml --activate-profiles release -DskipTests -DskipITs deploy
+mvn --batch-mode --settings ci/settings.xml --activate-profiles release -DskipTests -DskipITs deploy
+MAVEN_EXIT_CODE="${?}"
 
 ## Clean up the GPG directory, so we don't leave anything behind
 find ci/gpg -mindepth 1 -delete
 
 ## Return the maven exit code, so we can report if maven failed
-MAVEN_EXIT_CODE="${?}"
 if [ ${MAVEN_EXIT_CODE} -ne 0 ]; then
     echo
     echo "Maven exited with error code [${MAVEN_EXIT_CODE}]."
