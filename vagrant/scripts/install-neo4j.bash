4fa1c844c0829bf7355746dfbdff5cae4271f587
James Bacon <james@baconi.co.uk>
2016-05-29 18:42:21 +0100

    Moved to using docker on travis-ci

--- vagrant/scripts/install-neo4j.bash
+++ vagrant/scripts/install-neo4j.bash
@@ -20,15 +20,24 @@ sudo apt-get --assume-yes install neo4j=${NEO4J_VERSION}
 sed -i 's/^# *dbms.connector.bolt.address=0.0.0.0:7687/dbms.connector.bolt.address=0.0.0.0:7687/' /etc/neo4j/neo4j.conf
 sed -i 's/^# *dbms.connector.http.address=0.0.0.0:7474/dbms.connector.http.address=0.0.0.0:7474/' /etc/neo4j/neo4j.conf
 
+AUTH_HEADER="Authorization: Basic $(echo -n neo4j:neo4j | base64)"
+ACCEPT_HEADER="Accept: application/json; charset=UTF-8"
+
+echo 'Waiting for Neo4j to have started: '
+until curl -s -H "${AUTH_HEADER}" -H "${ACCEPT_HEADER}" 'http://localhost:7474/user/neo4j'; do
+    echo -n '.'
+    sleep 1s
+done
+
 ## Change default user:password from [neo4j/neo4j] http://neo4j.com/docs/stable/rest-api-security.html
 curl -X POST \
   -d '{ "password" : "password" }' \
-  -H "Authorization: Basic $(echo -n neo4j:neo4j | base64)" \
-  -H 'Accept: application/json; charset=UTF-8' \
+  -H "${AUTH_HEADER}" \
+  -H "${ACCEPT_HEADER}" \
   -H 'Content-Type: application/json' \
   'http://localhost:7474/user/neo4j/password'
 
 ## Restart Neo4J to pick up configuration changes.
-sudo /etc/init.d/neo4j-service restart
+sudo systemctl restart neo4j
 
 touch /home/vagrant/.install-neo4j-has-run
