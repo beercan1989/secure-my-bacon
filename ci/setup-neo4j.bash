923c41caee24c59056a4213107cc3f32d06dd089
James Bacon <james@baconi.co.uk>
2016-05-29 16:22:40 +0100

    Neo4j start up script improvement.
    Might cause an infinite loop in its current form.

--- ci/setup-neo4j.bash
+++ ci/setup-neo4j.bash
@@ -11,13 +11,25 @@ tar -xzf neo4j-community-${NEO4J_VERSION}-unix.tar.gz
 ## Start Neo4J
 neo4j-community-${NEO4J_VERSION}/bin/neo4j start
 
-## Give it an extra second or two
-sleep 2s
+AUTH_HEADER="Authorization: Basic $(echo -n neo4j:neo4j | base64)"
+ACCEPT_HEADER="Accept: application/json; charset=UTF-8"
+
+## Wait until Neo4j server has started
+function neo4j-responding {
+    curl -H "Authorization: Basic $(echo -n neo4j:neo4j | base64)" \
+         -H 'Accept: application/json; charset=UTF-8' \
+         'http://localhost:7474/user/neo4j'
+}
+echo 'Waiting for Neo4j to have started: '
+until curl -H "${AUTH_HEADER}" -H "${ACCEPT_HEADER}" 'http://localhost:7474/user/neo4j'; do
+    echo -n '.'
+    sleep 1s
+done
 
 ## Reset Neo4J Password
 curl -X POST \
     -d '{ "password" : "password" }' \
-    -H "Authorization: Basic $(echo -n neo4j:neo4j | base64)" \
-    -H 'Accept: application/json; charset=UTF-8' \
+    -H "${AUTH_HEADER}" \
+    -H "${ACCEPT_HEADER}" \
     -H 'Content-Type: application/json' \
     'http://localhost:7474/user/neo4j/password'
