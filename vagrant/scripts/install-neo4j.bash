d3b308917acd3b003f762d5b36a96a4eca4b3d82
James Bacon <james@baconi.co.uk>
2016-05-29 14:53:35 +0100

    Updated vagrant config to install specific neo4j

--- vagrant/scripts/install-neo4j.bash
+++ vagrant/scripts/install-neo4j.bash
@@ -8,12 +8,13 @@ fi
 
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
+export NEO4J_VERSION="2.3.4"
 
 wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
 sudo tee /etc/apt/sources.list.d/neo4j.list <<< "deb http://debian.neo4j.org/repo stable/"
 sudo apt-get update
 
-sudo apt-get --assume-yes install neo4j
+sudo apt-get --assume-yes install neo4j=${NEO4J_VERSION}
 
 ## Enable external connections
 sed -i 's/#org.neo4j.server.webserver.address=0.0.0.0/org.neo4j.server.webserver.address=0.0.0.0/' /var/lib/neo4j/conf/neo4j-server.properties
