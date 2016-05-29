489f3218a394eb193f2038ee46d4d24596b100bf
James Bacon <james@baconi.co.uk>
2016-05-29 18:42:21 +0100

    Moved to using docker on travis-ci

--- ci/setup-neo4j.bash
+++ ci/setup-neo4j.bash
@@ -2,37 +2,17 @@
 
 set -e
 
-## Download Neo4J
-wget http://dist.neo4j.org/neo4j-community-${NEO4J_VERSION}-unix.tar.gz
+DOCKER_NAME='secure-my-bacon-neo4j-3'
 
-## Extract Neo4J
-tar -xzf neo4j-community-${NEO4J_VERSION}-unix.tar.gz
+## Create and start docker container
+docker run --detach --name ${DOCKER_NAME} --publish=7474:7474 --publish=7687:7687 --env=NEO4J_AUTH=neo4j/password neo4j:3.0.1
 
-## Start Neo4J
-neo4j-community-${NEO4J_VERSION}/bin/neo4j start
-
-## Tail the Neo4j server logs, TODO - Remove once working
-tail -n+0 -F neo4j-community-3.0.1/logs/neo4j.log &
-
-AUTH_HEADER="Authorization: Basic $(echo -n neo4j:neo4j | base64)"
+AUTH_HEADER="Authorization: Basic $(echo -n neo4j:password | base64)"
 ACCEPT_HEADER="Accept: application/json; charset=UTF-8"
 
 ## Wait until Neo4j server has started
-function neo4j-responding {
-    curl -H "Authorization: Basic $(echo -n neo4j:neo4j | base64)" \
-         -H 'Accept: application/json; charset=UTF-8' \
-         'http://localhost:7474/user/neo4j'
-}
 echo 'Waiting for Neo4j to have started: '
-until curl -H "${AUTH_HEADER}" -H "${ACCEPT_HEADER}" 'http://localhost:7474/user/neo4j'; do
+until curl -s -H "${AUTH_HEADER}" -H "${ACCEPT_HEADER}" 'http://localhost:7474/user/neo4j'; do
     echo -n '.'
     sleep 1s
 done
-
-## Reset Neo4J Password
-curl -X POST \
-    -d '{ "password" : "password" }' \
-    -H "${AUTH_HEADER}" \
-    -H "${ACCEPT_HEADER}" \
-    -H 'Content-Type: application/json' \
-    'http://localhost:7474/user/neo4j/password'
