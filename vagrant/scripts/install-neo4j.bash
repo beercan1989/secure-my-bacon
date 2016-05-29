43002a7db9bb452a2605bd87540f1690e3760dd2
James Bacon <james@baconi.co.uk>
2016-05-29 16:04:47 +0100

    Changes to use Neo4j v3.x

--- vagrant/scripts/install-neo4j.bash
+++ vagrant/scripts/install-neo4j.bash
@@ -8,7 +8,7 @@ fi
 
 export DEBIAN_FRONTEND=noninteractive
 export DEBCONF_NONINTERACTIVE_SEEN=true
-export NEO4J_VERSION="2.3.4"
+export NEO4J_VERSION="3.0.1"
 
 wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
 sudo tee /etc/apt/sources.list.d/neo4j.list <<< "deb http://debian.neo4j.org/repo stable/"
@@ -17,7 +17,8 @@ sudo apt-get update
 sudo apt-get --assume-yes install neo4j=${NEO4J_VERSION}
 
 ## Enable external connections
-sed -i 's/#org.neo4j.server.webserver.address=0.0.0.0/org.neo4j.server.webserver.address=0.0.0.0/' /var/lib/neo4j/conf/neo4j-server.properties
+sed -i 's/^# *dbms.connector.bolt.address=0.0.0.0:7687/dbms.connector.bolt.address=0.0.0.0:7687/' /etc/neo4j/neo4j.conf
+sed -i 's/^# *dbms.connector.http.address=0.0.0.0:7474/dbms.connector.http.address=0.0.0.0:7474/' /etc/neo4j/neo4j.conf
 
 ## Change default user:password from [neo4j/neo4j] http://neo4j.com/docs/stable/rest-api-security.html
 curl -X POST \
