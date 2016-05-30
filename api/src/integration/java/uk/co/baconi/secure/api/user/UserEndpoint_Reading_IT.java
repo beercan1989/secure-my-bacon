6ec6d7b87b8c75353715b14373c61621c3fe0d14
James Bacon <james@baconi.co.uk>
2016-05-30 10:15:48 +0100

    New user, find all paginated, find by id.

--- api/src/integration/java/uk/co/baconi/secure/api/user/UserEndpoint_Reading_IT.java
+++ api/src/integration/java/uk/co/baconi/secure/api/user/UserEndpoint_Reading_IT.java
@@ -16,9 +16,12 @@
 
 package uk.co.baconi.secure.api.user;
 
+import com.jayway.restassured.http.ContentType;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import uk.co.baconi.secure.api.integrations.IntegratedApiEndpoint;
+
+import java.io.IOException;
 
 import static org.hamcrest.Matchers.*;
 
@@ -33,11 +36,59 @@ public class UserEndpoint_Reading_IT extends IntegratedApiEndpoint {
 
                 then().assertThat().
 
-                body(".", is(not(emptyCollectionOf(String.class)))).
+                body("data", is(not(emptyCollectionOf(String.class)))).
 
-                body("[0].name", isA(String.class)).
+                body("data[0].id", isA(Number.class)).
+                body("data[0].name", isA(String.class)).
+
+                body("paging.page", isA(Integer.class)).
+                body("paging.perPage", isA(Integer.class)).
+                body("paging.totalCount", isA(Integer.class)).
+
+                body("data[0].id", is(equalTo(1))).
+                body("data[0].name", is(equalTo("user-0"))).
+
+                body("paging.page", is(equalTo(0))).
+                body("paging.perPage", is(equalTo(5))).
 
                 statusCode(is(equalTo(HttpStatus.OK.value())));
     }
+
+    @Test
+    public void onCreateNewUser() throws IOException {
+
+        withNoAuthentication().
+                baseUri(getBaseUrl()).
+                contentType(ContentType.JSON).
+                body(convertToJson(new NewUser("new-clean-user"))).
+                post("/users").
+
+                then().assertThat().
+
+                body("id", isA(Number.class)).
+                body("name", isA(String.class)).
+
+                statusCode(is(equalTo(HttpStatus.OK.value())));
+    }
+
+    @Test
+    public void onFindUserById() throws IOException {
+
+        withNoAuthentication().
+                baseUri(getBaseUrl()).
+                get("/users/by-id/{id}", 1).
+
+                then().assertThat().
+
+                body("id", isA(Number.class)).
+                body("name", isA(String.class)).
+
+                body("id", is(equalTo(1))).
+                body("name", is(equalTo("user-0"))).
+
+                statusCode(is(equalTo(HttpStatus.OK.value())));
+    }
+
+    // Find by name
 
 }
--- api/src/main/java/uk/co/baconi/secure/api/user/UserEndpoint.java
+++ api/src/main/java/uk/co/baconi/secure/api/user/UserEndpoint.java
@@ -16,33 +16,89 @@
 
 package uk.co.baconi.secure.api.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import uk.co.baconi.secure.base.bag.Bag;
+import uk.co.baconi.secure.base.pagination.PaginatedResult;
 import uk.co.baconi.secure.base.user.User;
 import uk.co.baconi.secure.base.user.UserGraphRepository;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+@Validated
 @RestController
 @RequestMapping(value = "/users", produces = "application/json; charset=UTF-8")
 public class UserEndpoint {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserEndpoint.class);
 
     @Autowired
     private UserGraphRepository userGraphRepository;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> get() {
+    public ResponseEntity<PaginatedResult<User>> get(
+            @Min(value = 0, message = "{uk.co.baconi.secure.api.Page.min}")
+            @RequestParam(required = false, defaultValue = "0") final Integer page,
 
-        final List<User> allUsers = StreamSupport.
-                stream(userGraphRepository.findAll().spliterator(), false).
-                collect(Collectors.toList());
+            @Min(value = 1, message = "{uk.co.baconi.secure.api.PerPage.min}")
+            @Max(value = 20, message = "{uk.co.baconi.secure.api.PerPage.max}")
+            @RequestParam(required = false, defaultValue = "5") final Integer perPage
+    ) {
 
-        return ResponseEntity.ok(allUsers);
+        final Page<User> paged = userGraphRepository.findAll(new PageRequest(page, perPage));
+
+        LOG.trace("paged: {}", paged);
+
+        final PaginatedResult<User> paginatedResult = new PaginatedResult<>(paged);
+
+        LOG.trace("paginatedResult: {}", paginatedResult);
+
+        return ResponseEntity.ok(paginatedResult);
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<User> create(@RequestBody final NewUser newUser){
+
+        LOG.trace("createUser: {}", newUser);
+
+        final User user = userGraphRepository.save(new User(newUser.getName()));
+
+        LOG.trace("createdUser: {}", user);
+
+        return ResponseEntity.ok(user);
+    }
+
+    @RequestMapping("/by-id/{userId}")
+    public ResponseEntity<User> findById(@PathVariable("userId") final Long id){
+
+        LOG.trace("findById: {}", id);
+
+        final User user = userGraphRepository.findOne(id);
+
+        LOG.trace("foundUser: {}", user);
+
+        return ResponseEntity.ok(user);
+    }
+
+//    @RequestMapping("/by-name/{userName}")
+//    public ResponseEntity<User> findByName(@PathVariable("userName") final String name){
+//
+//        LOG.trace("findByName: {}", name);
+//
+//        final User user = userGraphRepository.findByName(name);
+//
+//        LOG.trace("foundUser: {}", user);
+//
+//        return ResponseEntity.ok(user);
+//    }
 }
