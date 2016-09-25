c9bf8709e46171e946ae0c3974a042bf96cd1c12
James Bacon <james@baconi.co.uk>
2016-09-25 17:27:20 +0100

    Updated BagEndpoint to start enabling Bag creation ... along with everything else involed in that actions.

--- api/src/main/java/uk/co/baconi/secure/api/bag/BagEndpoint.java
+++ api/src/main/java/uk/co/baconi/secure/api/bag/BagEndpoint.java
@@ -24,28 +24,32 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uk.co.baconi.secure.base.bag.Bag;
 import uk.co.baconi.secure.base.bag.BagGraphRepository;
+import uk.co.baconi.secure.base.lock.AsymmetricLock;
+import uk.co.baconi.secure.base.lock.AsymmetricLockGraphRepository;
 import uk.co.baconi.secure.base.pagination.PaginatedResult;
+import uk.co.baconi.secure.base.user.User;
+import uk.co.baconi.secure.base.user.UserGraphRepository;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
 @Slf4j
 @Validated
 @RestController
-@AllArgsConstructor(onConstructor=@__({@Autowired}))
+@AllArgsConstructor(onConstructor = @__({@Autowired}))
 @RequestMapping(value = "/bags", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public class BagEndpoint {
 
     private final BagGraphRepository bagGraphRepository;
+    private final UserGraphRepository userGraphRepository;
+    private final AsymmetricLockGraphRepository asymmetricLockGraphRepository;
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<PaginatedResult<Bag>> findAllPaginated(
+    public ResponseEntity<PaginatedResult<Bag>> findAll(
             @Min(value = 0, message = "{uk.co.baconi.secure.api.Page.min}")
             @RequestParam(required = false, defaultValue = "0") final Integer page,
 
@@ -65,4 +69,26 @@ public class BagEndpoint {
         return ResponseEntity.ok(paginatedResult);
     }
 
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<Bag> create(@Valid @RequestBody final NewBag newBag) {
+
+        log.trace("createBag: {}", newBag);
+
+        // Find user who this bag will initially belong to
+        final User user = userGraphRepository.findByName(newBag.getUserName());
+        log.trace("foundUser: {}", user);
+
+        // TODO - Generate bags key pair
+        final byte[] publicKey = {1, 2, 3, 4};
+        final byte[] privateKey = {5, 6, 7, 8};
+
+        final Bag bag = bagGraphRepository.save(new Bag(newBag.getBagName(), publicKey));
+        log.trace("createdBag: {}", bag);
+
+        // TODO - Secure private key with User details
+        final AsymmetricLock lock = asymmetricLockGraphRepository.save(new AsymmetricLock(bag, user, privateKey));
+        log.trace("createdAsymmetricLock: {}", lock);
+
+        return ResponseEntity.ok(bag);
+    }
 }
