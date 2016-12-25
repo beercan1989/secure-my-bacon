1b4cb585c7aae8548b1aef65a0a69419fd30d3af
James Bacon <james@baconi.co.uk>
2016-12-25 12:03:03 +0000

    Added PowerMock to enable testing of the mian method in ApiApplication.

---
+++ api/src/test/java/uk/co/baconi/secure/api/ApiApplicationTest.java
@@ -0,0 +1,40 @@
+package uk.co.baconi.secure.api;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(SpringApplication.class)
+public class ApiApplicationTest {
+
+    @Test
+    public void shouldRunSpringApplicationViaMain() {
+
+        // Setup static mock
+        PowerMockito.mockStatic(SpringApplication.class);
+
+        ApiApplication.main("Test Argument");
+
+        // Verify static is called
+        PowerMockito.verifyStatic(Mockito.times(1));
+        SpringApplication.run(Mockito.eq(ApiApplication.class), Mockito.eq("Test Argument"));
+    }
+
+    @Test
+    public void shouldCreateMethodValidationPostProcessorBean() {
+
+        final Object underTest = new ApiApplication().methodValidationPostProcessor();
+
+        assertThat(underTest).isInstanceOf(MethodValidationPostProcessor.class);
+    }
+
+}
