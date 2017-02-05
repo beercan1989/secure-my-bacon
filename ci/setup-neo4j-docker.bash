b3ddada80bc00e41d9c9bf5d588a8159c17d852b
James Bacon <james@baconi.co.uk>
2017-02-05 12:57:39 +0000

    Scripts to setup various neo4j servers in CI.

--- ci/setup-neo4j-docker.bash
+++ ci/setup-neo4j-docker.bash
@@ -2,10 +2,11 @@
 
 set -e
 
+NEO4J_VERSION="${1:-3.0.1}"
 DOCKER_NAME='secure-my-bacon-neo4j-3'
 
 ## Create and start docker container
-docker run --detach --name ${DOCKER_NAME} --publish=7474:7474 --publish=7687:7687 --env=NEO4J_AUTH=neo4j/password neo4j:3.0.1
+docker run --detach --name ${DOCKER_NAME} --publish=7474:7474 --publish=7687:7687 --env=NEO4J_AUTH=neo4j/password neo4j:${NEO4J_VERSION}
 
 AUTH_HEADER="Authorization: Basic $(echo -n neo4j:password | base64)"
 ACCEPT_HEADER="Accept: application/json; charset=UTF-8"
